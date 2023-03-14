53cef0883589f1371f07e5a7bdf1039f59555d6d
thesis <1711327451@qq.com>
2023-03-14 18:43:30 +0800

    1 全局配置样式；2 完成首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,151 +1,256 @@
 <template>
-  <view class="container">
-    <view class="title">{{$t('index.demo')}}</view>
-    <view class="description">{{$t('index.demo-description')}}</view>
-    <view class="detail-link">{{$t('index.detail')}}: <text
-        class="link">https://uniapp.dcloud.net.cn/collocation/i18n</text></view>
-    <view class="locale-setting">{{$t('index.language-info')}}</view>
-    <view class="list-item">
-      <text class="k">{{$t('index.system-language')}}:</text>
-      <text class="v">{{systemLocale}}</text>
-    </view>
-    <view class="list-item">
-      <text class="k">{{$t('index.application-language')}}:</text>
-      <text class="v">{{applicationLocale}}</text>
-    </view>
-    <view class="locale-setting">{{$t('index.language')}}</view>
-    <view class="locale-list">
-      <view class="locale-item" v-for="(item, index) in locales" :key="index" @click="onLocaleChange(item)">
-        <text class="text">{{item.text}}</text>
-        <text class="icon-check" v-if="item.code == applicationLocale"></text>
-      </view>
-    </view>
-  </view>
+	<view class="container">
+		<view class="header">
+			<image class="bg" src="https://img-shop.qmimg.cn/s23107/2020/04/26/3eb7808bf105262604.jpg"></image>
+			<view class="intro d-flex flex-column">
+				<view class="font-size-lg text-color-white mb-10">您好，微信用户</view>
+				<view class="font-size-sm text-color-white">一份物品，由仓管理开启您的家居物品管理</view>
+			</view>
+		</view>
+		<view class="content">
+			<view class="take-in-out d-flex mb-30">
+				<view class="item d-flex flex-column justify-content-center align-items-center">
+					<image class="icon" src="/static/images/index/zq.png"></image>
+					<text class="title">存入</text>
+				</view>
+				<view class="item d-flex flex-column justify-content-center align-items-center">
+					<image class="icon" src="/static/images/index/wm.png"></image>
+					<text class="title">取出</text>
+				</view>
+			</view>
+			<view class="integral d-flex align-items-center mb-30">
+				<view class="integral-section flex-fill">
+					<view class="d-flex align-items-center">
+						<text class="font-size-base text-color-base" style="margin-right: 10rpx;">我的积分</text>
+						<text class="bold">{{ userIntegral || 411 }}</text>
+					</view>
+					<view class="d-flex align-items-center">
+						<view class="font-size-sm text-color-assist">进入积分商城兑换券及周边好礼</view>
+						<view class="iconfont iconarrow-right"></view>
+					</view>
+				</view>
+				<view class="qrcode-section d-flex flex-column align-items-center">
+					<image class="img mb-10" src="/static/images/index/qrcode.png"></image>
+					<text class="font-size-sm text-color-primary">会员码</text>
+				</view>
+			</view>
+			<view class="activities mb-20">
+				<view class="left">
+					<view class="grid">
+						<view class="d-flex align-items-center">
+							<image class="mark-img" src="/static/images/index/csc.png"></image>
+							<text class="font-size-sm text-color-base">仓管理小商城</text>
+						</view>
+						<view class="tooltip text-color-assist">海量优质好我，网红公仔</view>
+					</view>
+					<view class="grid"><image class="shop-img" src="/static/images/index/yzclh.png" mode="heightFix"></image></view>
+				</view>
+				<view class="right d-flex flex-column font-size-sm text-color-base">
+					<view class="tea-activity mb-20">
+						<image class="icon" src="/static/images/index/mcsb.png"></image>
+						<text>买茶送包</text>
+						<view class="right-img"><image src="/static/images/index/mcsb_bg.png" mode="widthFix"></image></view>
+					</view>
+					<view class="member-coupon">
+						<image class="icon" src="/static/images/index/hyjb.png"></image>
+						<text>会员券包</text>
+						<view class="right-img"><image src="/static/images/index/hyjb_bg.png" mode="widthFix"></image></view>
+					</view>
+				</view>
+			</view>
+			<view class="member mb-30">
+				<view class="title d-flex justify-content-between align-items-center">
+					<view class="font-size-lg font-weight-bold">会员新鲜事</view>
+					<view class="iconfont iconRightbutton text-color-assist" style="font-size: 52rpx;"></view>
+				</view>
+				<view class="list">
+					<view class="item" v-for="(item, idx) in memberStoryList" :key="idx">
+						<image class="bg-img" :src="item.imgSrc"></image>
+						<text class="item-title">{{ item.title }}</text>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        systemLocale: '',
-        applicationLocale: ''
-      }
-    },
-    computed:{
-      locales() {
-        return [{
-            text: this.$t('locale.auto'),
-            code: 'auto'
-          }, {
-            text: this.$t('locale.en'),
-            code: 'en'
-          },
-          {
-            text: this.$t('locale.zh-hans'),
-            code: 'zh-Hans'
-          },
-          {
-            text: this.$t('locale.zh-hant'),
-            code: 'zh-Hant'
-          },
-          {
-            text: this.$t('locale.ja'),
-            code: 'ja'
-          }
-        ]
-      }
-    },
-    onLoad() {
-      let systemInfo = uni.getSystemInfoSync();
-      this.systemLocale = systemInfo.language;
-      this.applicationLocale = uni.getLocale();
-      this.isAndroid = systemInfo.platform.toLowerCase() === 'android';
-      uni.onLocaleChange((e) => {
-        this.applicationLocale = e.locale;
-      })
-    },
-    methods: {
-      onLocaleChange(e) {
-        if (this.isAndroid) {
-          uni.showModal({
-            content: this.$t('index.language-change-confirm'),
-            success: (res) => {
-              if (res.confirm) {
-                uni.setLocale(e.code);
-              }
-            }
-          })
-        } else {
-          uni.setLocale(e.code);
-          this.$i18n.locale = e.code;
-        }
-      }
-    }
-  }
+export default {
+	data() {
+		return {
+			memberStoryList: [{ imgSrc: 'https://img-shop.qmimg.cn/s23107/2020/04/27/0039bf41c9ebd50a2c.jpg', title: '"梅"你不行 | 霸气杨梅清爽回归' }]
+		};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-  .title {
-    font-size: 16px;
-    font-weight: bold;
-    margin-bottom: 15px;
-  }
-
-  .description {
-    font-size: 14px;
-    opacity: 0.6;
-    margin-bottom: 15px;
-  }
-
-  .detail-link {
-    font-size: 14px;
-    word-break: break-all;
-  }
-
-  .link {
-    color: #007AFF;
-    margin-left: 10px;
-  }
-
-  .locale-setting {
-    font-size: 16px;
-    font-weight: bold;
-    margin-top: 25px;
-    margin-bottom: 5px;
-    padding-bottom: 5px;
-    border-bottom: 1px solid #f0f0f0;
-  }
-
-  .list-item {
-    font-size: 14px;
-    padding: 10px 0;
-  }
-
-  .list-item .v {
-    margin-left: 5px;
-  }
-
-  .locale-item {
-    display: flex;
-    flex-direction: row;
-    padding: 10px 0;
-  }
-
-  .locale-item .text {
-    flex: 1;
-  }
-
-  .icon-check {
-    margin-right: 5px;
-    border: 2px solid #007aff;
-    border-left: 0;
-    border-top: 0;
-    height: 12px;
-    width: 6px;
-    transform-origin: center;
-    /* #ifndef APP-NVUE */
-    transition: all 0.3s;
-    /* #endif */
-    transform: rotate(45deg);
-  }
+<style scoped lang="scss">
+.container {
+	background-color: $bg-color;
+	.header {
+		position: relative;
+		width: 100%;
+		height: 600rpx;
+		.bg {
+			width: 100%;
+			height: 600rpx;
+			position: absolute;
+			top: 0;
+		}
+		.intro {
+			position: absolute;
+			left: 40rpx;
+			top: calc(50rpx + var(--status-bar-height));
+		}
+	}
+	.content {
+		position: relative;
+		padding: 0 30rpx;
+		.take-in-out {
+			padding: 30rpx 0;
+			background-color: #fff;
+			border-radius: 10rpx;
+			box-shadow: box-shadow;
+			position: relative;
+			margin-top: -80rpx;
+			.item {
+				width: 49%;
+				position: relative;
+				.icon {
+					width: 84rpx;
+					height: 84rpx;
+					margin: 20rpx;
+				}
+				.title {
+					font-size: 30rpx;
+					color: $text-color-base;
+					font-weight: 600;
+				}
+				&.item:nth-child(1)::after {
+					content: '';
+					width: 1rpx;
+					height: 100%;
+					background-color: #ddd;
+					position: absolute;
+					right: 0;
+					transform: scaleX(0.5) scaleY(0.8);
+				}
+			}
+		}
+		.integral {
+			padding: 30rpx;
+			background-color: #fff;
+			border-radius: 10rpx;
+			box-shadow: box-shadow;
+			.integral-section {
+				.bold {
+					font-size: 44rpx;
+					font-weight: 700;
+				}
+			}
+			.qrcode-section {
+				.img {
+					width: 40rpx;
+					height: 40rpx;
+				}
+			}
+		}
+		.activities {
+			padding: 20rpx;
+			background-color: #fff;
+			border-radius: 10rpx;
+			box-shadow: box-shadow;
+			display: flex;
+			align-items: stretch;
+			.left {
+				width: 340rpx;
+				padding: 0 20rpx;
+				margin-right: 20rpx;
+				background-color: $bg-color-primary;
+				.grid:nth-last-child(1) {
+					align-items: flex-end;
+				}
+				.grid {
+					height: 50%;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					.tooltip {
+						font-size: 10px;
+						margin-left: 40rpx;
+					}
+					.mark-img {
+						width: 30rpx;
+						height: 30rpx;
+						margin-right: 10rpx;
+					}
+					.shop-img {
+						height: 123rpx;
+					}
+				}
+			}
+			.right {
+				width: 290rpx;
+				.right-img {
+					flex: 1;
+					position: relative;
+					margin-left: 20rpx;
+					margin-right: -20rpx;
+					margin-bottom: -20rpx;
+					display: flex;
+					align-items: flex-end;
+					image {
+						width: 100%;
+					}
+				}
+				.icon {
+					width: 30rpx;
+					height: 30rpx;
+					margin-right: 10rpx;
+				}
+				.tea-activity {
+					padding: 20rpx;
+					display: flex;
+					align-items: center;
+					position: relative;
+					background-color: #fdf3f2;
+				}
+				.member-coupon {
+					padding: 20rpx;
+					display: flex;
+					align-items: center;
+					position: relative;
+					background-color: #fcf6d4;
+				}
+			}
+		}
+		.member {
+			.title {
+				padding: 20rpx 0;
+			}
+			.list {
+				width: 100%;
+				display: flex;
+				flex-direction: column;
+				.item {
+					width: 100%;
+					height: 240rpx;
+					.bg-img {
+						width: 100%;
+						height: 100%;
+						border-radius: 8rpx;
+					}
+					.item-title {
+						font-size: $font-size-lg;
+						color: #fff;
+						position: relative;
+						top: -70rpx;
+						left: 10rpx;
+					}
+				}
+			}
+		}
+	}
+}
 </style>
